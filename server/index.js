36952525f0dfcf04407610a90ccaaa59f72b9ac5
Mohit Pardeshi <mohitpardeshi729@gmail.com>
2023-09-24 15:29:58 +0530

    changed the server file location

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const { configDotenv } = require("dotenv");
+const express = require("express");
+const app = express();
+configDotenv();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
